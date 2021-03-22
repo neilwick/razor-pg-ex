598974295b55d59e1f061a2586b241d890dc00a3
Neil Wick <neil@wick.net>
2021-03-22 10:43:17 -0400

    initial setup

---
+++ Models/Game.cs
@@ -0,0 +1,15 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace razor_pg_ex.Models {
+
+    public class Game {
+        public int GameID { get; set;}
+        public string Title { get; set; }
+
+        [DataType (DataType.Date)]
+        public DateTime ReleaseDate {get; set; }
+        public decimal Price {get; set; }
+
+    }
+}
